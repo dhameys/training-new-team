e425f5de046af4a6028c97de5eff559c57ac7319
immoazhar <azharmm6866@gmail.com>
2024-01-18 14:13:41 +0300

    adding ui kits pannel

--- src/views/ui kits/alerts/index.vue
+++ src/views/ui kits/alerts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    page sale
+    page alerts
   </div>
 </template>
 
@@ -10,6 +10,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
--- src/views/ui kits/badges/index.vue
+++ src/views/ui kits/badges/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    page sale
+    page badge
   </div>
 </template>
 
