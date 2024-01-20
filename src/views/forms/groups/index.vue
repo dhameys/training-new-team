8e6ee07867deeaae85d4a7877f9cf025fe8376bd
farahsane <hsomali66@gmail.com>
2024-01-20 09:38:45 +0300

    adding form

--- src/views/forms/groups/index.vue
+++ src/views/forms/groups/index.vue
@@ -1,15 +1,64 @@
 <template>
-    <section class="w-full p-4 bg-Gray-400 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-700 flex flex-row justify-self-auto flex-wrap items-center">
+    <section class="w-full p-4 bg-gray-100 border border-gray-200 shadow sm:p-6 md:p-8 dark:border-gray-700 flex flex-row flex-wrap justify-between">
 
         <!-- ================================================== Input Groups ====================================== -->
-        <div class="w-1/2 div h-auto p-3 bg-white">
-            <div class=" font-bold tracking-tight text-gray-900 ">
-               <h1>Input Groups</h1>
-               <p>Place one add-on or button on either side of an input. You may also place one on both sides of an input. Remember to place <span class="text-red-600">labels</span> outside the input group.</p>
-               <div class="w-full ">
-                <button>@</button> <input type="username" placeholder="Username"/>
-               </div>
-            </div>
+        <div class="container py-8 px-4 bg-white w-1/2 h-auto shadow-xl">
+            <form action="">
+                <h1>Input Groups</h1>
+                <label class="flex flex-col mb-6">
+                  <span class="mb-2 inline-block cursor-pointer ">Place one add-on or button on either side of an input. You may also place one on both sides of an input. Remember to place labels outside the input group.</span>
+                  <div class="flex">
+                          <button class="bg-gray-500 text-white rounded-l  px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@</button>
+                          <input class="p-4 rounded-r leading-none w-full" type="text" placeholder="Username">
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                    <h1 class="mb-3">Your vanity URL</h1>
+                  <div class="flex">
+                    <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >https://example.com/users/</button><input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> 
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >$</button><input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >.00</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >$0.00</button><input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> 
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+                <label class="flex flex-col mb-6">
+                  <div class="flex">
+                    <input class="p-4 rounded-l leading-none w-full" type="text" placeholder="Recipient's username"> <button class="bg-gray-500 text-white rounded-r px-8 transition hover:bg-blue-dark focus:bg-blue-dark" >@example.com</button>
+                  </div>
+                </label>
+            </form>
         </div>
 
         <!-- ============================================= Buttons with dropdowns ======================================================= -->
@@ -52,3 +101,4 @@ export default {}
 </script>
 
 <style lang="scss" scoped></style>
+
