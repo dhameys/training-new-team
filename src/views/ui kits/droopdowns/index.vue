996f53eb11182f7372100d323ee1d9db9fca053e
immoazhar <azharmm6866@gmail.com>
2024-01-20 21:12:57 +0300

    update in ui kits

--- src/views/ui kits/droopdowns/index.vue
+++ src/views/ui kits/droopdowns/index.vue
@@ -1,15 +1,90 @@
 <template>
-  <div>
-  
-  </div>
+    <div class="flex gap-2">
+        <div class="block bg-white w-96 h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="text-xl">Dropdown 1</p>
+
+            <button class="text-white mt-10 bg-gray-600 rounded-lg p-2">Dropdown button</button>
+        </div>
+
+        <div class="block bg-white w-96 h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="text-xl">Dropdown 2</p>
+
+            <button class="text-white mt-10 bg-gray-600 rounded-lg p-2">Dropdown link</button>
+        </div>
+
+        <div class="block bg-white w-96 h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="text-xl">Dropdown 3</p>
+
+            <button class="text-white mt-10 bg-gray-600 rounded-lg p-2">Dropdown link</button>
+        </div>
+    </div>
+    <br />
+
+    <div class="block bg-white w-full h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="text-xl mb-12">Dropdown 4</p>
+
+        <button type="button" class="text-white bg-danger focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Action</button>
+        <button
+            type="button"
+            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium bg-gray-700 text-white focus:outline-none rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+            Action
+        </button>
+        <button type="button" class="text-white bg-emerald-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Action</button>
+        <button
+            type="button"
+            class="text-gray-900 border border-gray-300 bg-sky-500 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+            Action
+        </button>
+        <button type="button" class="focus:outline-none bg-warning text-white focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Action</button>
+        <button type="button" class="focus:outline-none bg-danger text-white focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Action</button>
+    </div>
+
+    <br />
+
+    <div class="block bg-white w-full h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="text-xl mb-12">Dropdown 5</p>
+
+        <button type="button" class="text-white bg-danger hover:bg-red-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Action</button>
+        <button
+            type="button"
+            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium bg-gray-700 hover:bg-gray-800 text-white focus:outline-none rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+            Action
+        </button>
+        <button type="button" class="text-white bg-emerald-600 focus:outline-none hover:bg-green-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Action</button>
+        <button
+            type="button"
+            class="text-gray-900 border border-gray-300 bg-sky-500 hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+            Action
+        </button>
+        <button type="button" class="focus:outline-none bg-warning hover:bg-amber-700 text-white focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Action</button>
+        <button type="button" class="focus:outline-none bg-danger hover:bg-red-900 text-white focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Action</button>
+    </div>
+    <br />
+
+    <div class="flex gap-2">
+        <div class="block bg-white w-1/3 h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="text-xl">Dropdown 6</p>
+
+            <button class="text-white mt-10 bg-gray-500 rounded p-2">
+                Right-aligned menu
+               
+            </button>
+        </div>
+
+        <div class="block bg-white w-full h-40 px-10 py-5 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p class="text-xl">Dropdown 7</p>
+
+            <button class="text-white mt-10 bg-gray-500 rounded p-2">Left-aligned but right when large screen</button>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
